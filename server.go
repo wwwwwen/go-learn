5a42eafb38f5dc6312134ddb8567cee8fca56246
xu <123>
2023-01-16 00:43:09 +0800

    v0.3用户消息广播功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -60,6 +61,51 @@ func (server *Server) Handler(conn net.Conn) {
 	//广播当前用户上线消息
 	server.BroadCast(user, "log in")
 
+	//接受客户端消息
+	//这个goroutine好像有点多余
+	go func() {
+		//close connection
+		defer func(conn net.Conn) {
+			fmt.Println("断开连接 与", conn.RemoteAddr().String())
+			err := conn.Close()
+			if err != nil {
+				fmt.Println("TCP connection close err:", err.Error())
+			}
+		}(conn)
+		char := make([]byte, 1)
+		for {
+			buf := make([]byte, 0, 4096)
+			//windows telnet的bug，一次只能发一个字符
+			//而且ctrl+]然后send sth. 不会发最后的回车
+			for {
+				num, err := conn.Read(char)
+				if num == 0 {
+					server.BroadCast(user, "log out")
+					return
+				}
+				if err != nil && err != io.EOF {
+					fmt.Println("Conn Read err:", err.Error())
+					return
+				}
+				//但是回车竟然是发两个字符 \r\num
+				if char[0] == '\r' || char[0] == '\n' {
+					break
+				} else {
+					buf = append(buf, char[0])
+				}
+			}
+			if len(buf) == 0 || char[0] == '\n' {
+				continue
+			}
+			//提取消息
+			msg := string(buf[0:])
+
+			//广播消息
+			server.BroadCast(user, msg)
+
+		}
+	}()
+
 	//handler阻塞
 	//select {}
 }
@@ -71,7 +117,12 @@ func (server *Server) Start() {
 		fmt.Printf("net.Listen err: %v\n", err)
 	}
 	//close listen socket
-	defer listener.Close()
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println("listen socket close err:", err.Error())
+		}
+	}(listener)
 
 	//启动监听Message的goroutine
 	go server.ListenMessage()
